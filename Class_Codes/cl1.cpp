f9d8ee9d1e2a7ba2567f4a6cdb4eb6870338c536
AhmedShafique313 <2021ee313@student.uet.edu.pk>
2024-02-16 09:53:33 +0500

    code 1 done

--- Class_Codes/cl1.cpp
+++ Class_Codes/cl1.cpp
@@ -37,3 +37,15 @@ double rect::getarea() const
 {
     return lenght * width;
 }
+
+int main()
+{
+    rect box;
+    double rect_l, rect_W;
+    cout<<"Enter the length of rectangle: ";cin>>rect_l;cout<<endl;
+    cout<<"ENter the width of rectangle: ";cin>>rect_W;cout<<endl;
+    box.setlength(rect_l);
+    box.setwidth(rect_W);
+    cout<<"/n The dimension of rectabgle like length "<<rect_l<<" meter and "<<"widht "<<rect_W<<" meter gives us area which is "<<box.getarea()<<" meter-square"<<endl;
+    return 0;
+}
