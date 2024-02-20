934003b5dcee09c24eaaed866e97e61835699b96
Ahmed Shafique <2021ee313@student.uet.edu.pk>
2024-02-21 00:12:28 +0500

    fun time

--- DSA_lab/filehand1.cpp
+++ DSA_lab/filehand1.cpp
@@ -17,3 +17,13 @@ void fun()
 }
 
 // main function here
+int main()
+{
+    clock_t t1,t2;
+    t1 = clock();
+    fun();
+    t2 = clock() - t1;
+    double time_taken = ((double) t2)/ CLOCKS_PER_SEC;
+    cout<<"fun() took of seconds to execute \n"<<time_taken;
+    return 0;
+}
