527064671cdae06ce077e76d1246a2cbea91894d
Ahmed Shafique <2021ee313@student.uet.edu.pk>
2024-02-28 00:56:18 +0500

    all done

--- DSA_lab/matrix_complete.cpp
+++ DSA_lab/matrix_complete.cpp
@@ -92,8 +92,41 @@ element substraction_matrces(int r1, int c1, int r2, int c2, int *p1, int *p2)
     return matrix;
 }
 
-void printing_matrix(int row, int col, int *ptr)
+void printing_matrix(int row, int col, int *ptr, string name)
 {
+    cout<<"Dear "<<name<<"! "<<"Matrix X has following contents: "<<endl;
+    for (int i = 0; i < row; i++)
+    {
+        cout << "[";
+        for (int j = 0; j < col; j++)
+        {
+            cout << " " << *ptr; // Corrected dereferencing here
+            ptr = ptr + 1;
+        }
+        cout << " ]" << endl;
+    }
+    return;
+}
+
+void add_printing_matrix(int row, int col, int *ptr, string name)
+{
+    cout<<"Dear "<<name<<"! "<<"Z = X + Y is shown below:"<<endl;
+    for (int i = 0; i < row; i++)
+    {
+        cout << "[";
+        for (int j = 0; j < col; j++)
+        {
+            cout << " " << *ptr; // Corrected dereferencing here
+            ptr = ptr + 1;
+        }
+        cout << " ]" << endl;
+    }
+    return;
+}
+
+void sub_printing_matrix(int row, int col, int *ptr, string name)
+{
+    cout<<"Dear "<<name<<"! "<<"Z = X - Y is shown below:"<<endl;
     for (int i = 0; i < row; i++)
     {
         cout << "[";
@@ -111,27 +144,30 @@ void printing_matrix(int row, int col, int *ptr)
 int main()
 {
     int r1, r2, c1, c2, *p1, *p2;
-    cout<<"Matrix 1"<<endl;
-    cout<<"Rows?: ";cin>>r1;cout<<endl;
-    cout<<"Columns?: ";cin>>c1;cout<<endl;
+    string user;
+    cout<<"Enter name of current user?: ";cin>>user;cout<<endl;
+    cout<<"Dear "<<user<<"! "<<"Tell me what are the dimensions of X?: "<<endl;
+    cin>>r1;
+    cin>>c1;
     p1 = new int [r1*c1];
+    cout<<"Dear "<<user<<"! "<<"Enter the contents of X:"<<endl;
     initialize_matrix(r1,c1,p1);
-    printing_matrix(r1,c1,p1);
+    printing_matrix(r1,c1,p1, user);
 
-    cout<<"Matrix 2"<<endl;
-    cout<<"Rows?: ";cin>>r2;cout<<endl;
-    cout<<"Columns?: ";cin>>c2;cout<<endl;
+    cout<<"Dear "<<user<<"! "<<"Tell me what are the dimensions of Y?: "<<endl;
+    cin>>r2;
+    cin>>c2;
     p2 = new int [r2*c2];
+    cout<<"Dear "<<user<<"! "<<"Enter the contents of Y:"<<endl;
     initialize_matrix(r2,c2,p2);
-    printing_matrix(r2,c2,p2);
+    printing_matrix(r2,c2,p2,user);
 
     element matrix1, matrix2;
     matrix1 = addition_matrces(r1,c1,r2,c2,p1,p2);
-    printing_matrix(matrix1.r, matrix1.c, matrix1.p);
+    add_printing_matrix(matrix1.r, matrix1.c, matrix1.p, user);
 
     matrix2 = substraction_matrces(r1,c1,r2,c2,p1,p2);
-    printing_matrix(matrix2.r, matrix2.c, matrix2.p);
-
+    sub_printing_matrix(matrix2.r, matrix2.c, matrix2.p, user);
     delete [] p1;
     delete [] p2;
     return 0;
