f9c4bd85c54fb377d79afb151b02fee29f8ac08f
Ahmed Shafique <2021ee313@student.uet.edu.pk>
2024-03-03 11:27:16 +0500

    matrix multiplication is going to start

---
+++ matrix_multi.cpp
@@ -0,0 +1,13 @@
+// row1 of matrix one should be equals to column of matrix two
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <new>
+#include <fstream>
+#include <time.h>
+#include <string.h>
+#include <ctime>
+using namespace std;
+
+
